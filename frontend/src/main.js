c59a740b4f7b42fb021d862f01dfd9cfee1bc921
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-05-12 21:54:31 -0300

    .

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import router from './router'
+import vuetify from './plugins/vuetify' // <- importação do Vuetify
 
 Vue.config.productionTip = false
 
-Vue.use(Vuetify)
-
 new Vue({
-  vuetify: new Vuetify({}),
+  router,
+  vuetify, // <- aqui você adiciona o Vuetify à instância
   render: h => h(App)
 }).$mount('#app')
