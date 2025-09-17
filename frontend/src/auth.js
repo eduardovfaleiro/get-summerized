9ef052983c5498d3ada5153fafab6bc783f0e555
Eduardo Faleiro <eduardoviniciusfaleiro@gmail.com>
2025-09-17 15:06:21 -0300

    Movido "isTokenExpired" para auth.js

--- frontend/src/auth.js
+++ frontend/src/auth.js
@@ -1,5 +1,6 @@
 // src/auth.js
 import { reactive } from 'vue';
+import { jwtDecode } from 'jwt-decode';
 
 export const auth = reactive({
     token: localStorage.getItem('access_token') || null,
@@ -12,3 +13,11 @@ export const auth = reactive({
         localStorage.removeItem('access_token');
     },
 });
+
+export function isTokenExpired(token) {
+  if (!token) return true;
+  const { exp } = jwtDecode(token);
+  if (!exp) return true;
+  const now = Date.now() / 1000; // timestamp em segundos
+  return exp < now;
+}
